4222183e7a0e0ea2987e21eb4022019778691e09
Lukesh Kawde <lkawde11@gmail.com>
2026-07-18 21:48:04 +0530

    Implement number positivity check in Task2.java
    The code contains multiple commented-out Java methods for various number checks and comparisons. The active code checks if a number is positive, negative, or zero.

---
+++ Task2.java
@@ -0,0 +1,168 @@
+import java.util.Scanner;
+
+//1.Check whether a number is positive or negative.
+/*public class task2{
+	public static void main(String[] args){
+		int a=5;
+		if( a>= 0){
+			System.out.println("Number is positive");
+		}else{
+			System.out.println("Number is Negative");
+		}
+	}
+}*/
+
+	
+//2.Check whether a number is even or odd.
+/*public class task2{
+	public static void main(String[] args){
+		int num=16;
+		if(num % 2 == 0){
+			System.out.println("Number is even");
+		}else{
+			System.out.println("Number is odd");
+		}
+	}
+}*/
+
+
+//3.Find the greater of two numbers.
+/*public class task2{
+	public static void main(String [] args){
+		int a=5,b=10;
+		if(a<b){
+			System.out.println("b is greater number ");
+		}else{
+			System.out.println("a  is greater number");
+		}
+	}
+}*/
+
+//4.Find the smaller of two numbers.
+/*public class task2{
+	public static void main(String [] args){
+		int a=5,b=10;
+		if(a>b){
+			System.out.println("b is smaller number");
+		}else{
+			System.out.println("a  is smaller number");
+		}
+	}
+}*/
+
+//5.Check whether a number is zero or not.
+/*public class task2{
+	public static void main(String [] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number");
+		int a = sc.nextInt();
+		if(a == 0){
+			System.out.println("A number is Zero");
+		}else{
+			System.out.println("number is not zero");
+		}
+	}
+}
+*/
+ 
+// 6.Check whether a number is divisible by 5.
+/*public class task2{
+	public static void main(String [] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number");
+		int num = sc.nextInt();
+		if(num % 5 == 0){
+			System.out.println("A number is divisible by 5");
+		}else{
+			System.out.println("A number is  not divisible by 5");
+		}
+	}
+}*/
+
+
+//7.Check whether a number is divisible by 11.
+/*public class task2{
+	public static void main(String [] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number");
+		int num = sc.nextInt();
+		if(num % 11 == 0){
+			System.out.println("A number is divisible by 11");
+		}else{
+			System.out.println("A number is  not divisible by 11");
+		}
+	}
+}*/
+
+//8.Check whether a number is a multiple of 3.
+/*public class task2{
+	public static void main(String [] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number");
+		int num = sc.nextInt();
+		if(num % 3 == 0){
+			System.out.println("A number is multiple by 3");
+		}else{
+			System.out.println("A number is  not multiple by 3");
+		}
+	}
+}*/
+//9.Check whether a number is a multiple of 5.
+/*public class task2{
+	public static void main(String [] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number");
+		int num = sc.nextInt();
+		if(num % 3 == 0){
+			System.out.println("A number is multiple by 3");
+		}else{
+			System.out.println("A number is  not multiple by 3");
+		}
+	}
+}*/
+
+//10.Check whether a number is positive, negative, or zero.
+public class task2{
+	public static void main(String [] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number");
+		int num = sc.nextInt();
+		if(num == 0){
+			System.out.println("Number is zero ");
+		}else if(num < 0){
+			System.out.println("number is negative ");
+		}else{
+			System.out.println("number is positive");
+		}
+	}
+}
+//11.Compare two numbers and print which is greater.
+
+//12.Compare two numbers and print which is smaller.
+
+//13.Check whether a number is greater than 100.
+
+//14.Check whether a number is less than 50.
+
+//15Check whether a number is equal to 25.
+
+//16Check whether a number is divisible by 2 and 3.
+
+//17.Check whether a number is divisible by 7.
+
+//18.Check whether a number is between 1 and 100.
+
+//19.Check whether a character is a vowel.
+
+//20.Check whether a character is a consonant.
+
+//21.Check whether a character is uppercase.
+
+//22Check whether a character is lowercase.
+
+//23Check whether a character is an alphabet.
+
+//24.Check whether a character is a digit.
+
+//25.Check whether a year is a leap year.
+
